1c1fc5abc5bc4fc4d79626815e2a6a9e00a610d8
YJ-Arch <yj1516268@outlook.com>
2023-02-16 13:44:50 +0800

    使用corba前

--- Software/rolling-count/go-version/rolling-count/rolling-count.go
+++ Software/rolling-count/go-version/rolling-count/rolling-count.go
@@ -19,6 +19,41 @@ import (
 )
 
 var fileName = "/var/log/pacman.log"
+var line = "--------------------------------------------------"
+
+// 获取系统安装时间和当前时间
+var lineText = ReadFileLine(fileName, 1)
+var startTimeStrLong = strings.Split(lineText, " [")[0]
+var startTimeStr = strings.Split(strings.Split(lineText, "[")[1], "]")[0]
+var currentTimeStr = time.Now().Format("2006-01-02 15:04")
+
+// 获取初始和当前内核版本
+var keyText = ReadFileKey(fileName, "upgraded linux ")
+var firstKernel = strings.Split(strings.Split(keyText, " (")[1], " ")[0]
+var latestKernel = RunCommand("uname", "-r")
+
+// 计算系统安装天数
+var local, _ = time.LoadLocation("Asia/Shanghai")
+var startTime, _ = time.ParseInLocation("2006-01-02 15:04", startTimeStr, local)
+var startTimeStamp = startTime.Unix()
+var currentTime, _ = time.ParseInLocation("2006-01-02 15:04", currentTimeStr, local)
+var currentTimeStamp = currentTime.Unix()
+var systemDays = (currentTimeStamp - startTimeStamp) / 86400
+
+// 获取系统/内核更新相关数据
+var systemUpdateCount = ReadFileCount(fileName, "starting full system upgrade")
+var systemUpdateMean = float32(systemUpdateCount) / float32(systemDays)
+var kernelUpdateCount = ReadFileCount(fileName, "upgraded linux ")
+var kernelUpdateMean = float32(systemDays) / float32(kernelUpdateCount)
+
+// 获取吉祥物
+var mascot = RunCommand("repo-elephant", "")
+
+// 参数变量
+var (
+	h, help    bool
+	v, version bool
+)
 
 // 读取文件指定行
 func ReadFileLine(file string, line int) string {
@@ -108,35 +143,7 @@ func RunCommand(command, args string) string {
 	return strings.TrimRight(outStr, "\n")
 }
 
-// 获取系统安装时间和当前时间
-var lineText = ReadFileLine(fileName, 1)
-var startTimeStrLong = strings.Split(lineText, " [")[0]
-var startTimeStr = strings.Split(strings.Split(lineText, "[")[1], "]")[0]
-var currentTimeStr = time.Now().Format("2006-01-02 15:04")
-
-// 获取初始和当前内核版本
-var keyText = ReadFileKey(fileName, "upgraded linux ")
-var firstKernel = strings.Split(strings.Split(keyText, " (")[1], " ")[0]
-var latestKernel = RunCommand("uname", "-r")
-
-// 计算系统安装天数
-var local, _ = time.LoadLocation("Asia/Shanghai")
-var startTime, _ = time.ParseInLocation("2006-01-02 15:04", startTimeStr, local)
-var startTimeStamp = startTime.Unix()
-var currentTime, _ = time.ParseInLocation("2006-01-02 15:04", currentTimeStr, local)
-var currentTimeStamp = currentTime.Unix()
-var systemDays = (currentTimeStamp - startTimeStamp) / 86400
-
-// 获取系统/内核更新相关数据
-var systemUpdateCount = ReadFileCount(fileName, "starting full system upgrade")
-var systemUpdateMean = float32(systemUpdateCount) / float32(systemDays)
-var kernelUpdateCount = ReadFileCount(fileName, "upgraded linux ")
-var kernelUpdateMean = float32(systemDays) / float32(kernelUpdateCount)
-
-// 获取吉祥物
-var mascot = RunCommand("repo-elephant", "")
-
-func main() {
+func Output() {
 	fmt.Printf("[%16v] %-2v [%-16v]\n", startTimeStr, "--", currentTimeStr)
 	fmt.Printf("%18v %-2v %-18v\n", firstKernel, "--", latestKernel)
 	fmt.Printf("%12v %-2v %-4.3v %v\n", "系统使用时长", "--", systemDays, "天")
@@ -145,4 +152,15 @@ func main() {
 	fmt.Printf("%12v %-2v %-4.3v %v\n", "内核更新次数", "--", kernelUpdateCount, "次")
 	fmt.Printf("%12v %-2v %-4.3v %v\n", "内核更新频率", "--", kernelUpdateMean, "天/次")
 	fmt.Println(mascot)
+}
+
+// func FlagUsage() {
+//     fmt.Fprint(os.Stdout, "rolling-count用于统计系统安装和更新信息\n"+line+"\nUsage: \n\n  rolling-count [-hv]\n\nOptions:\n")
+//     flag.BoolVar(&h, "h", false, "显示此帮助信息并退出")
+//     flag.BoolVar(&v, "v", false, "显示版本信息并退出")
+// }
+
+
+func main() {
+	Output()
 }
